3a7eb6d25880679b7f1e1ed356ce7a76e60ff7df
qkrgusdn1 <baghyeonu127@gmail.com>
2024-06-22 17:37:30 +0900

    클리어 카운트

--- Assets/2. Script/Mission/BrickOut/BrickBall.cs
+++ Assets/2. Script/Mission/BrickOut/BrickBall.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BrickBall : MonoBehaviour
 {
     public float speed;
     public Vector2 direction;
     public LayerMask wallLayerMask;
+    Vector3 position;
 
+    private void Start()
+    {
+        position = transform.position;
+    }
     private void Update()
     {
         direction = direction.normalized;
+        
 
         RaycastHit2D hit2d = Physics2D.Raycast(transform.position, direction, Time.deltaTime * speed, wallLayerMask);
 
@@ -19,6 +26,10 @@ public class BrickBall : MonoBehaviour
             if (hit2d.collider.CompareTag("Brick"))
             {
                 hit2d.collider.gameObject.SetActive(false);
+            }
+            else if (hit2d.collider.CompareTag("ResetBall"))
+            {
+                transform.position = position;
             }
             direction = Vector2.Reflect(direction, hit2d.normal);
         }
